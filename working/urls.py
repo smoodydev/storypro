43daae155a527e2b2f204725fb652b473f23f135
Stephen Moody <darilligames@gmail.com>
2019-11-07 00:02:25 +0000

    added book to display titles

--- working/urls.py
+++ working/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
-from .views import workspaces, createworkspace, createbook, createchapter, workspace
+from .views import workspaces, createworkspace, createbook, createchapter, workspace, book
 
 urlpatterns = [
     url(r'^$', workspaces, name="workspaces"),
     url(r'^createworkspace', createworkspace, name="createworkspace"),
     url(r'^createbook', createbook, name="createbook"),
     url(r'^createchapter', createchapter, name="createchapter"),
-    url(r'^workspace/(\d+)', workspace, name="workspace"),
+    url(r'^workspace/(\d+)/$', workspace, name="workspace"),
+    url(r'^workspace/(\d+)/(\d+)/$', book, name="book"),
 
 
 ]
--- working/views.py
+++ working/views.py
@@ -20,6 +20,12 @@ def createchapter(request):
     return render(request, "createworkspace.html", {"form":form})
 
 def workspace(request, id):
+    print("doing this")
     content = get_object_or_404(Workspace, pk=id)
     books = Book.objects.filter(workspace=content.id)
     return render(request, "workspace.html", {"content":content, "books":books})
+
+def book(request, ws, id):
+    content = get_object_or_404(Workspace, pk=ws)
+    book = get_object_or_404(Book, pk=id)
+    return render(request, "book.html", {"content":content, "book":book})
